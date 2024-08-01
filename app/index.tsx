7644f27fd7a0748aa151f7f263e8c38df668f78b
John <126916249+johnsvd105@users.noreply.github.com>
2024-08-01 16:45:53 -0600

    File structure change, drawer implemented
    file structure changed. Renamed App to index, added drawer for navigation, updated package to work with drawer

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
-import Button from './components/Button';
-import Display from './components/Display';
-import History from './components/History';
-import { styles } from './styles/styles';
-
+import Button from '../components/Button';
+import Display from '../components/Display';
+import History from '../components/History';
+import { styles } from '../styles/styles';
 export default function App() {
   const [displayValue, setDisplayValue] = useState('0');
   const [operator, setOperator] = useState(null);
@@ -92,3 +91,39 @@ export default function App() {
     </View>
   );
 }
+
+// import React from 'react';
+// import { View, Text } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//     </View>
+//   );
+// }
+
+// function DetailsScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Details Screen</Text>
+//     </View>
+//   );
+// }
+
+// const Stack = createNativeStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Home">
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Details" component={DetailsScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
